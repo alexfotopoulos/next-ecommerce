f579ec14940a94f2bb376fda6c62592f2bde2b1f
Alex Fotopoulos <alex.foto.dev@gmail.com>
2023-05-09 11:14:47 -0400

    add and implement sass variables throughout project

--- components/collectionPage/SneakerTile.js
+++ components/collectionPage/SneakerTile.js
@@ -8,8 +8,8 @@ export default function SneakerTile(props) {
             <div className={styles.SneakerTile}>
                 <img className={styles.SneakerTileImage} src={props.images[0]} alt={props.title} />
                 <div>
-                    <h3>{props.title}</h3>
-                    <h4>${props.price}</h4>
+                    <div>{props.title}</div>
+                    <div>${props.price}</div>
                 </div>
             </div>
         </Link>
